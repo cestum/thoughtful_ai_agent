10a91b5aae8fa88d894586c983060026d0619257
Prem Rajendran <dev@me.prem.pro>
2024-12-20 19:33:59 -0800

    working prototype

--- agent.py
+++ agent.py
@@ -6,6 +6,14 @@ import streamlit as st
 from llm_client import LLMClient
 from rag_builder import RAGBuilder
 
+#ok, sample agent template requries work
+AGENT_TEMPLATE=(
+    "You are Thoughtful.ai company's AI agent trained to answer user questions on Thoughtful and Thoughtful healthcare products."
+    "The answers to given questions may be given directly in the context along with trained relevent question."
+    "If you find an answer in context content use that answer directly. "
+    "If the question is not related to Thoughtful and healthcare products, please answer to best of your knowledge."
+    "{context}"
+)
 def init_llm_client():
    return LLMClient()
 
@@ -48,12 +56,21 @@ def start_streamlit_session():
         for answer in answers:
             #use answer as primary content?
             context = context + f"; Content: {answer.metadata.get("answer")} \n" + f" Related question: {answer.page_content}" 
+        system_template_content = AGENT_TEMPLATE.format(context = context)
 
-        with st.chat_message("agent"):
+        # we may need to introduce a system template here for retrieval.
+        # use a separate internal_message array to pass it to llm
+        interal_messages = []
+        for m in st.session_state.messages:
+            interal_messages.append({"role": m["role"], "content": m["content"]})
+        #add agent context
+        interal_messages.append({"role":"system", "content": system_template_content})
+
+        with st.chat_message("assistant"):
             message_placeholder = st.empty()
             text = ""
             #use llm client to get chat completion response
-            text_stream = st.session_state.llmclient.get_stream(st.session_state.messages)
+            text_stream = st.session_state.llmclient.get_stream(interal_messages)
             for chunk in text_stream:
                 content = chunk.choices[0].delta.content
                 if content is not None: text += content #accumalate response
@@ -61,7 +78,7 @@ def start_streamlit_session():
             message_placeholder.markdown(text)
 
         #save agent answer to history
-        st.session_state.messages.append({"role":"agent", "content": text})
+        st.session_state.messages.append({"role":"assistant", "content": text})
 
 #main code
 if __name__ == "__main__":
